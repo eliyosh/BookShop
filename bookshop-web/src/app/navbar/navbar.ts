c8d76fefccb573b0de1ac2613f964f51decd5e2a
eliyosh <20246799@s.uabguio.edu>
2026-06-16 12:20:23 +0800

    feat: build a navbar

--- bookshop-web/src/app/navbar/navbar.ts
+++ bookshop-web/src/app/navbar/navbar.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import {RouterLink, RouterLinkActive} from '@angular/router';
+import { Cart } from '../cart';
+
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  standalone:true,
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar {}
+export class Navbar {
+
+  public martService = inject(Cart);
+  cartCount = this.martService.cartItemCount;
+}
