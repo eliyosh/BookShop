07660bb98e9f2613107d4eb7c9446b59f90a8043
eliyosh <20246799@s.ubaguio.edu>
2026-06-18 07:01:14 +0800

    feat: updated the app ts

--- bookshop-web/src/app/app.ts
+++ bookshop-web/src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './navbar/navbar';
+import { CartService } from './cart';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('bookshop-web');
+  cartService = inject(CartService);
 }
